1559e8abc5e5a4ee34e0d54717f558a78bff4a29
31611389 <31611389@099214-P31S202.macklabs.br>
2017-05-10 09:33:50 -0300

    "adição do construtor de RegistroDaAula"

--- Academia/src/academia/RegistroDaAula.java
+++ Academia/src/academia/RegistroDaAula.java
@@ -5,14 +5,29 @@
  */
 package academia;
 
+import java.util.Date;
+
 /**
  *
  * @author Sr.Adulis
  */
 public class RegistroDaAula {
+    private Date data;
+    private int hora;
     private String objetivo;
-    private boolean efetuada;
+    private String tipo;
+    
 
+    public RegistroDaAula(Date data, int hora, String objetivo, String tipo) {
+        this.data = data;
+        this.hora = hora;
+        this.objetivo = objetivo;
+        this.tipo = tipo;
+        
+    }
+    
+    
+    
     public String getObjetivo() {
         return objetivo;
     }
@@ -21,12 +36,27 @@ public class RegistroDaAula {
         this.objetivo = objetivo;
     }
 
-    public boolean isEfetuada() {
-        return efetuada;
+    public Date getData() {
+        return data;
     }
 
-    public void setEfetuada(boolean efetuada) {
-        this.efetuada = efetuada;
+    public void setData(Date data) {
+        this.data = data;
     }
-    
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }

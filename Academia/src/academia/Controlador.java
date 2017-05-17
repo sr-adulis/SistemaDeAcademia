fdf931104e8b94eee4a2568e8f0220ee314ca735
Pedro Henrique <Pedro Henrique@LAPTOP-UP0MQKII>
2017-05-16 21:27:05 -0300

    iniciando criação do Menu de opções academia/Controlador

--- Academia/src/academia/Controlador.java
+++ Academia/src/academia/Controlador.java
@@ -5,6 +5,8 @@
  */
 package academia;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Pedro Henrique
@@ -14,21 +16,34 @@ public class Controlador {
     static Personal[] personal = new Personal[5];
 
     public static void menuPrincipal() {
-        Personal[] personal = new Personal[5];
-
+        Scanner in = new Scanner(System.in);
+        String menuP;
         System.out.println("Projeto agendamento de academia Windows academic");
-        System.out.println("\n MENU PRINCIPAL"
-                + "\n - Lista de Personal treiners "
-                
-                + printListaPersonal()
-                + "\n 2- buscar Personais disponiveis por data");
+        System.out.println("\n MENU PRINCIPAL"/*Mostra Todos + opções:selecionar(remover/alterar/adicionar/adicionarCompromisso)*/
+                + "\n 1- Lista de Personal treiners "
+                + "\n 2- Adicionbar novo personal"
+                + "\n 2- Buscar Personais disponiveis por data"/*Mostra somente os disponiveis + opções : selecionar(adicionar compromisso)*/
+                + "");
+        System.out.println("Enter com a opção desejada");
+        menuP = in.nextLine();
+        switch (menuP) {
+            case ("1"):
+                printListaPersonal();
+            case("2"):    
+        }
 
+    }
+
+    public static void addPersonal() {
+        System.out.println("Opção adicionar personal selecionada");
+        Scanner in = new Scanner(System.in);
+        
     }
 
     public static String printListaPersonal() {
         if (Personal.getQuantidade() > 0) {
             int i = 0;
-            String resultado = "\n\n Nº do personal: " + i + "nome: " + personal[i].getNome() + "\n";
+            String resultado = "\n\n\n Nº do personal: " + i + "nome: " + personal[i].getNome() + "\n";
             i++;
             if (Personal.getQuantidade() > i) {
                 return resultado + printListaPersonal(i);
